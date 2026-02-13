6c112dd1aaf7c1713c7fc8611dd94af672b9793f
DiGiSaudia <digisaudia@gmail.com>
2026-02-13 22:07:34 +0500

    added contact page

---
+++ app/contact/page.js
@@ -0,0 +1,8 @@
+export default function Contact() {
+  return (
+    <div style={{ padding: "50px", textAlign: "center" }}>
+      <h1>Contact Us</h1>
+      <p>Email: info@saudiprice.com</p>
+    </div>
+  );
+}
